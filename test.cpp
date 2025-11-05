8d805cae57d414d8f244b864804e2d750cb07a63
Skyboy12 <105098128+Skyboy12@users.noreply.github.com>
2025-11-05 20:20:21 +0700

    Add .cpp files batch 2 (2/5)

--- test.cpp
+++ test.cpp
@@ -2,11 +2,31 @@
 
 using namespace std;
 
-int main(){
-    int n = 5;
-    cout << n << endl;
-    auto p = &n;
-    *p = 10;
-    cout << n << endl;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    long long total_pairs = 0;
+    int i = 0;
+    while (i < n) {
+        int j = i;
+        while (j < n && s[j] == s[i]) {
+            j++;
+        }
+
+        long long k = j - i;
+
+        total_pairs += k * (k - 1) / 2;
+
+        i = j;
+    }
+
+    cout << total_pairs << "\n";
+
     return 0;
 }
